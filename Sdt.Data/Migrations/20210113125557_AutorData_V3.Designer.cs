4e6e6e353f5fbfcffde17671bab99d3add460a4d
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-13 13:56:54 +0100

    Migration für Data

--- Sdt.Data/Migrations/20210113125557_AutorData_V3.Designer.cs
+++ Sdt.Data/Migrations/20210113125557_AutorData_V3.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Sdt.Data.Context;
 
 namespace Sdt.Data.Migrations
 {
     [DbContext(typeof(SdtDataContext))]
-    partial class SdtDataContextModelSnapshot : ModelSnapshot
+    [Migration("20210113125557_AutorData_V3")]
+    partial class AutorData_V3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
