b347edb2ab0af078a46aec556559f7d572a2fb13
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-13 14:59:05 +0100

    Autor Liste

---
+++ Sdt.Web.Mvc/Controllers/AutorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sdt.Data.Context;
+
+namespace Sdt.Web.Mvc.Controllers
+{
+    public class AutorController : Controller
+    {
+        private readonly SdtDataContext _context;
+
+        public AutorController(SdtDataContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var autoren = _context.Autoren.ToList();
+            return View(autoren);
+        }
+    }
+}
