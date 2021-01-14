2bcff92fc5fc324f1fdac8ef5d61c19190f9d630
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-14 15:27:36 +0100

    EmailTagHelper

---
+++ Sdt.Web.Common/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sdt.Web.Common.TagHelpers
+{
+    // <email display-name="admin" adresse="mailto:admin@admin.com">admin</email>
+    public class EmailTagHelper : TagHelper
+    {
+        public string DisplayName { get; set; }
+        public string Adresse { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a"; // ersetzt <email> mit <a>
+            var adresse = $"mailto:{Adresse}";
+            output.Attributes.SetAttribute("href", adresse);
+            output.Content.SetContent(DisplayName);
+        }
+    }
+}
