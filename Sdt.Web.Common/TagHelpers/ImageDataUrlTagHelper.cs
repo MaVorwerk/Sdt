562c50ed2f3810b0885ccc4d1c7dcb1f46f32260
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-14 16:00:25 +0100

    TagHelper

---
+++ Sdt.Web.Common/TagHelpers/ImageDataUrlTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Sdt.Web.Common.TagHelpers
+{
+    [HtmlTargetElement("imagedataurl")]
+    public class ImageDataUrlTagHelper : TagHelper
+    {
+        public string ImageMimeType { get; set; }
+        public byte[] Image { get; set; }
+        public string Width { get; set; } = "120";
+        public string AltSrc { get; set; } = "/images/noimg.jpg";
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "img"; // <img>
+            output.Attributes.SetAttribute("width", Width); // <img width="120">
+            var src = !string.IsNullOrEmpty(ImageMimeType) && Image?.Length > 0
+                ? $"data:{ImageMimeType};base64,{Convert.ToBase64String(Image)}"  
+                : AltSrc;
+
+            output.Attributes.SetAttribute("src", src); // <img src="data... oder <img src="/images/noimg.jpg
+            output.TagMode = TagMode.SelfClosing;
+        }
+    }
+}
