0d70e5b06839b2cca456eaaf10caec0afdc7289a
Ali Ilci <Ali.Ilci@gmx.de>
2021-01-15 11:25:04 +0100

    Alternative

---
+++ Sdt.Web.Mvc/Models/SdtSummaryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sdt.Web.Mvc.Models
+{
+    public class SdtSummaryViewModel
+    {
+        public int AnzahlAutoren { get; set; }
+        public int AnzahlSprueche { get; set; }
+    }
+}
---
+++ Sdt.Web.Mvc/ViewComponents/SdtSummaryViewComponent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Sdt.Data.Contracts;
+using Sdt.Web.Mvc.Models;
+
+namespace Sdt.Web.Mvc.ViewComponents
+{
+    public class SdtSummaryViewComponent : ViewComponent
+    {
+        private readonly IAutorRepository _autorRepository;
+
+        public SdtSummaryViewComponent(IAutorRepository autorRepository)
+        {
+            _autorRepository = autorRepository;
+        }
+
+        //1. Version ohne View
+        //public HtmlContentViewComponentResult Invoke()
+        //{
+        //    return new HtmlContentViewComponentResult(new HtmlString("Das ist eine <h3>ViewComponente</h3>"));
+        //}
+
+        //2. Version mit View
+        public IViewComponentResult Invoke()
+        {
+            var autoren = _autorRepository.GetAll().ToList();
+            var sdtSummaryVm = new SdtSummaryViewModel
+            {
+                AnzahlAutoren = autoren.Count,
+                AnzahlSprueche = autoren.Sum(c => c.Sprueche.Count)
+            };
+            
+            //return View(sdtSummaryVm);
+            return View("Alternative",sdtSummaryVm);
+        }
+    }
+}
